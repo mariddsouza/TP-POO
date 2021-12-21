90d5375145ec204ac7084727bcf457e9f1bb91cf
MateusAparecido <72242588+MateusAparecido@users.noreply.github.com>
2021-12-21 13:00:24 -0300

    add itens prof

--- src/DAO/DAOProfessor.java
+++ src/DAO/DAOProfessor.java
@@ -1,20 +1,20 @@
 package DAO;
-import model.ModelAluno;
+import model.ModelProfessor;
 import util.ConexaoSQLite;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class DAOAluno extends ConexaoSQLite {
-    public boolean salvarAlunoDAO(ModelAluno pModelAluno){
+public class DAOProfessor extends ConexaoSQLite {
+    public boolean salvarProfessorDAO(ModelProfessor pModelProfessor){
         conectar();
         String sql= "INSERT INTO tbl_aluno (" + "nome, " + "login, " + "senha) " + "VALUES  (?,?,?)";
         PreparedStatement preparedStatement = criarPreparedStatement(sql, Statement.RETURN_GENERATED_KEYS);
         try {
-            preparedStatement.setString(1, pModelAluno.getAlunoNome());
-            preparedStatement.setString(1, pModelAluno.getAlunoLogin());
-            preparedStatement.setString(1, pModelAluno.getAlunoSenha());
+            preparedStatement.setString(1, pModelProfessor.getProfNome());
+            preparedStatement.setString(1, pModelProfessor.getProfLogin());
+            preparedStatement.setString(1, pModelProfessor.getProfSenha());
             preparedStatement.executeQuery();
         } catch (SQLException ex) {
             Logger.getLogger(DAOAluno.class.getName()).log(Level.SEVERE, null, ex);
