f663c1a7f99eaa25cb4b93f8e15f80421cc0d06a
MateusAparecido <72242588+MateusAparecido@users.noreply.github.com>
2022-01-31 16:20:03 -0300

    s

--- src/poogrupo/PooGrupo.java
+++ src/poogrupo/PooGrupo.java
@@ -1,8 +1,10 @@
 
 package poogrupo;
-
+import view.Login;
 public class PooGrupo {
     public static void main(String[] args) {
+        new Login().setVisible(true);
+            
     }
     
 }
