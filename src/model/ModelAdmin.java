741032c88198fb1ab12616a4038463053c908d7e
MateusAparecido <72242588+MateusAparecido@users.noreply.github.com>
2022-03-24 18:54:58 -0300

    cadastro e login 100%

---
+++ src/model/ModelAdmin.java
@@ -0,0 +1,79 @@
+package model;
+public class ModelAdmin {
+    private int admId;
+    private String admNome;
+    private String admLogin;
+    private String admSenha;
+    private String admEmail;
+    /**
+     * @return the admId
+     */
+    public int getAdmId() {
+        return admId;
+    }
+
+    /**
+     * @param admId the admId to set
+     */
+    public void setAdmId(int admId) {
+        this.admId = admId;
+    }
+
+    /**
+     * @return the admNome
+     */
+    public String getAdmNome() {
+        return admNome;
+    }
+
+    /**
+     * @param admNome the admNome to set
+     */
+    public void setAdmNome(String admNome) {
+        this.admNome = admNome;
+    }
+
+    /**
+     * @return the admLogin
+     */
+    public String getAdmLogin() {
+        return admLogin;
+    }
+
+    /**
+     * @param admLogin the admLogin to set
+     */
+    public void setAdmLogin(String admLogin) {
+        this.admLogin = admLogin;
+    }
+
+    /**
+     * @return the admfSenha
+     */
+    public String getAdmSenha() {
+        return admSenha;
+    }
+
+    /**
+     * @param admfSenha the admfSenha to set
+     */
+    public void setAdmSenha(String admfSenha) {
+        this.admSenha = admfSenha;
+    }
+
+    /**
+     * @return the admEmail
+     */
+    public String getAdmEmail() {
+        return admEmail;
+    }
+
+    /**
+     * @param admEmail the admEmail to set
+     */
+    public void setAdmEmail(String admEmail) {
+        this.admEmail = admEmail;
+    }
+    
+ 
+}
